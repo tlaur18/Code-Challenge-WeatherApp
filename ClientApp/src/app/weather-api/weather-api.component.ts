8a38d54e82da28ad08765c108c8e0d4710f71958
tlaur18 <tlaur18@student.sdu.dk>
2020-01-15 13:41:25 +0100

    Weather-page added

---
+++ ClientApp/src/app/weather-api/weather-api.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-api',
+  templateUrl: './weather-api.component.html',
+  styleUrls: ['./weather-api.component.css']
+})
+export class WeatherApiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
