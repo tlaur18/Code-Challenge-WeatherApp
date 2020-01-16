160908b9c24a0cb37b3448d4c87fc253a56eeff4
tlaur18 <tlaur18@student.sdu.dk>
2020-01-16 10:52:06 +0100

    Introduced DTO to filter unimportant data

--- Controllers/WeatherApiController.cs
+++ Controllers/WeatherApiController.cs
@@ -6,6 +6,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyWeatherApp.DTOs;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace MyWeatherApp.Controllers
@@ -20,15 +22,19 @@ namespace MyWeatherApp.Controllers
         public async Task<IActionResult> GetWeather(string icaoCode)
         {
             var httpClient = new HttpClient();
-            HttpResponseMessage response = await httpClient.GetAsync(url + icaoCode);
-            if ((int)response.StatusCode >= 200 && (int)response.StatusCode < 300)
+            HttpResponseMessage apiResponse = await httpClient.GetAsync(url + icaoCode);
+            if ((int)apiResponse.StatusCode >= 200 && (int)apiResponse.StatusCode < 300)
             {
-                string jsonString = await response.Content.ReadAsStringAsync();
-                return Ok(jsonString);
+                string jsonString = await apiResponse.Content.ReadAsStringAsync();
+                WeatherDataDTO weatherData = JsonConvert.DeserializeObject<WeatherDataDTO>(jsonString);  //DTO anvendes til at sortere ligegyldigt data fra
+                JObject response = new JObject();
+                response.Add("conditions", JToken.FromObject(weatherData.report.conditions));
+                response.Add("forecast", JToken.FromObject(weatherData.report.forecast));
+                return Ok(response.ToString());
             }
             else
             {
-                throw new HttpListenerException((int)response.StatusCode, response.ReasonPhrase);
+                throw new HttpListenerException((int)apiResponse.StatusCode, apiResponse.ReasonPhrase);
             }
         }
     }
---
+++ DTOs/WeatherDataDTO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWeatherApp.DTOs
+{
+    public class WeatherDataDTO
+    {
+        public Report report { get; set; }
+    }
+
+    public class Report
+    {
+        public Conditions conditions { get; set; }
+        public Forecast forecast { get; set; }
+    }
+
+    public class Conditions
+    {
+        public string dateIssued { get; set; }
+        public double tempC { get; set; }
+        public double pressureHg { get; set; }
+        public Wind wind { get; set; }
+        public Visibility visibility { get; set; }
+    }
+
+    public class Forecast
+    {
+        public IList<Conditions> conditions { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speedKts { get; set; }
+        public double gustSpeedKts { get; set; }
+        public double direction { get; set; }
+    }
+
+    public class Visibility
+    {
+        public double distanceSm { get; set; }
+        public double distanceQualifier { get; set; }
+        public double prevailingVisSm { get; set; }
+        public double prevailingVisDistanceQualifier { get; set; }
+    }
+}
